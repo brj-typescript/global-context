5d871c6f4a4fdafed8f0301cfd868291886a35ea
Jan Barášek <janbarasek@gmail.com>
2023-04-23 12:20:46 +0200

    Add getStorage, setStorage and isKey.

--- src/useShareObject/index.ts
+++ src/useShareObject/index.ts
@@ -6,11 +6,21 @@ import { Theme } from '../theme/types';
 export const useShareObject = () => {
   const { getContext, setContext } = useContext(GlobalAppState);
 
+  const getStorage = <T>(key: string, initialState: T): T => (getContext() as Record<string, T>)[key] ?? initialState;
+
+  const setStorage = <T>(key: string, value: T) =>
+    setContext({
+      ...((getContext() as Record<string, T>)[key] ?? {}),
+      [key]: value,
+    });
+
+  const isKey = (key: string): boolean => Boolean(getStorage(key, false));
+
   const getTheme = (): Theme => getContext().theme || Theme.Light;
 
   const getUserIdentity = (): UserIdentity => ({ isLoggedIn: false });
 
   const isLoggedIn = (): boolean => getUserIdentity().isLoggedIn;
 
-  return { getContext, setContext, getTheme, getUserIdentity, isLoggedIn };
+  return { getContext, setContext, getStorage, setStorage, isKey, getTheme, getUserIdentity, isLoggedIn };
 };
